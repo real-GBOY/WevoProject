9b5f5cdb8503bffba4e470328f2c881ca4e01339
mahmoud nayel <mahmmoudnayel2004@gmail.com>
2025-06-14 19:45:58 +0300

    spacing at hero

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -95,15 +95,15 @@ const Hero = () => {
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				transition={{ delay: 1, duration: 0.8 }}
-				className='absolute bottom-8 left-1/2 transform -translate-x-1/2'>
+				className='absolute bottom-12 left-1/2 transform -translate-x-1/2 z-10'>
 				<motion.div
 					animate={{ y: [0, 10, 0] }}
 					transition={{ repeat: Infinity, duration: 2 }}
-					className='w-6 h-10 border-2 border-white rounded-full flex justify-center'>
+					className='w-6 h-10 border-2 border-white/50 rounded-full flex justify-center backdrop-blur-sm'>
 					<motion.div
 						animate={{ y: [0, 12, 0] }}
 						transition={{ repeat: Infinity, duration: 2 }}
-						className='w-1 h-3 bg-white rounded-full mt-2'
+						className='w-1 h-3 bg-white/80 rounded-full mt-2'
 					/>
 				</motion.div>
 			</motion.div>
